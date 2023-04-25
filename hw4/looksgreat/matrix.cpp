7e5e2e56e20feb0c0e9db9ee6feda4c285282d19
looksgreat <lemonade.cs07@nctu.edu.tw>
2023-04-26 00:53:05 +0800

    try

--- hw4/looksgreat/matrix.cpp
+++ hw4/looksgreat/matrix.cpp
@@ -99,78 +99,85 @@ private:
     vector<double, CustomAllocator<double>> m_data;
 };
 
-Matrix multiply_naive(Matrix const &m1, Matrix const &m2){
-    Matrix ret(m1.nrow(), m2.ncol());
-    for (int i = 0; i < m1.nrow(); i++) {
-        for (int j = 0; j < m2.ncol(); j++) {
+Matrix multiply_naive(const Matrix& a, const Matrix& b) {
+    if (a.ncol() != b.nrow())
+    {
+        throw std::out_of_range("matrix column differs from row size");
+    }
+
+    Matrix c(a.nrow(), b.ncol());
+
+    for (int i = 0; i < a.nrow(); ++i) {
+        for (int j = 0; j < b.ncol(); ++j) {
             double sum = 0.0;
-            for (int k = 0; k < m1.ncol(); k++) {
-                sum += m1(i, k) * m2(k, j);
+            for (int k = 0; k < a.ncol(); ++k) {
+                sum += a(i, k) * b(k, j);
             }
-            ret(i, j) = sum;
+            c(i, j) = sum;
         }
     }
-    return ret;
+
+    return c;
 }
 
-Matrix multiply_tile(Matrix const &m1, Matrix const &m2, int const tile_size){
-    int m = m1.nrow();
-    int n = m2.ncol();
-    int k = m1.ncol();
+Matrix multiply_tile(const Matrix& a, const Matrix& b, int tile_size) {
+    Matrix c(a.nrow(), b.ncol());
 
-    Matrix ret(m, n);
-    for (int i0 = 0; i0 < m; i0 += tile_size) {
-        int imax = std::min(i0 + tile_size, m);
-        for (int j0 = 0; j0 < n; j0 += tile_size) {
-            int jmax = std::min(j0 + tile_size, n);
-            for (int k0 = 0; k0 < k; k0 += tile_size) {
-                int kmax = std::min(k0 + tile_size, k);
-                for (int i = i0; i < imax; i++) {
-                    for (int j = j0; j < jmax; j++) {
+    for (int i0 = 0; i0 < a.nrow(); i0 += tile_size) {
+        int i1 = std::min(i0 + tile_size, a.nrow());
+        for (int j0 = 0; j0 < b.ncol(); j0 += tile_size) {
+            int j1 = std::min(j0 + tile_size, b.ncol());
+            for (int k0 = 0; k0 < a.ncol(); k0 += tile_size) {
+                int k1 = std::min(k0 + tile_size, a.ncol());
+                for (int i = i0; i < i1; ++i) {
+                    for (int j = j0; j < j1; ++j) {
                         double sum = 0.0;
-                        for (int l = k0; l < kmax; l++) {
-                            sum += m1(i, l) * m2(l, j);
+                        for (int k = k0; k < k1; ++k) {
+                            sum += a(i, k) * b(k, j);
                         }
-                        ret(i, j) += sum;
+                        c(i, j) += sum;
                     }
                 }
             }
         }
     }
 
-    return ret;
-    
+    return c;
 }
 
-Matrix multiply_mkl(Matrix const &m1, Matrix const &m2){
-    mkl_set_num_threads(1);
-    Matrix ret(m1.nrow(), m2.ncol());
-    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, m1.nrow(),  m2.ncol(), m1.ncol(), 1.0 , m1.data(),
-     m1.ncol(), m2.data(), m2.ncol(), 0.0, const_cast<double*>(ret.data()), ret.ncol());
-    return ret;
+Matrix multiply_mkl(const Matrix& a, const Matrix& b) {
+    Matrix c(a.nrow(), b.ncol());
+
+    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
+                a.nrow(), b.ncol(), a.ncol(), 1.0,
+                a.data(), a.ncol(), b.data(), b.ncol(),
+                0.0, const_cast<double*>(c.data()), c.ncol());
+
+    return c;
 }
 
 
-PYBIND11_MODULE(_matrix, m)
-{
-    m.doc() = "matrix multiplication testing";
-    m.def("multiply_naive", &multiply_naive);
-    m.def("multiply_tile", &multiply_tile);
-    m.def("multiply_mkl", &multiply_mkl);
+PYBIND11_MODULE(_matrix, m){
+    m.doc() = "matrix-matrix multiplication";
+    m.def("multiply_naive", &multiply_naive, "naive");
+    m.def("multiply_tile", &multiply_tile, "tile");
+    m.def("multiply_mkl", &multiply_mkl, "mkl");
     m.def("bytes", &CustomAllocator<double>::bytes);
     m.def("allocated", &CustomAllocator<double>::allocated);
     m.def("deallocated", &CustomAllocator<double>::deallocated);
-
-    pybind11::class_<Matrix>(m, "Matrix")
-        .def(pybind11::init<>())
-        .def(pybind11::init<int, int>())
+    py::class_<Matrix>(m, "Matrix")
+        .def(py::init<>())
+        .def(py::init<int, int>())
         .def("data", pybind11::overload_cast<>(&Matrix::data))
         .def("data", pybind11::overload_cast<>(&Matrix::data, pybind11::const_))
-        // .def("__call__", pybind11::overload_cast<int, int>(&Matrix::operator()), pybind11::return_value_policy::reference_internal)
-        // .def("__call__", pybind11::overload_cast<int, int>(&Matrix::operator(), pybind11::const_), pybind11::return_value_policy::reference_internal)
-        .def("__setitem__", [](Matrix &self, std::pair<int, int> id, double val) { self(id.first, id.second) = val; })
-        .def("__getitem__", [](const Matrix &self, std::pair<int, int> id) { return self(id.first, id.second); })
-        .def("__eq__", [](const Matrix &m1, const Matrix &m2) { return m1 == m2; })
         .def_property_readonly("nrow", [](const Matrix &mat) { return mat.nrow(); })
-        .def_property_readonly("ncol", [](const Matrix &mat) { return mat.ncol(); });
+        .def_property_readonly("ncol", [](const Matrix &mat) { return mat.ncol(); })
+        .def("__eq__", [](const Matrix &a, const Matrix &b) { 
+            return a == b; })
+        .def("__setitem__", [](Matrix &self, std::pair<int, int> idx, double val) {
+            self(idx.first, idx.second) = val;
+        })
+        .def("__getitem__", [](const Matrix &self, std::pair<int, int> idx) {
+            return self(idx.first, idx.second);
+        });
 }
