5ff6debcea7aa8a587da815837db50255c864dd1
looksgreat <lemonade.cs07@nctu.edu.tw>
2023-04-25 23:42:09 +0800

    fix

--- hw4/looksgreat/matrix.cpp
+++ hw4/looksgreat/matrix.cpp
@@ -47,7 +47,7 @@ class Matrix {
 
 
 public:
-    Matrix(): m_rows(0), m_cols(0){}
+    Matrix(): m_rows(0), m_cols(0), m_data(vector<double, CustomAllocator<double>>(0)){}
     Matrix(int rows, int cols) : m_rows(rows), m_cols(cols), m_data(vector<double, CustomAllocator<double>>(rows*cols)){}
     Matrix(const Matrix &m) : m_cols(m.ncol()), m_rows(m.nrow()){
         m_data = vector<double, CustomAllocator<double>>(m_cols*m_rows);
